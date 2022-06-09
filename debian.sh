dac806d01c48aa225c72bc86d89e991966293f6f
Dennis Dudek <admin@kapsonfire.de>
2022-06-09 09:09:54 +0200

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -2,7 +2,7 @@
 # Download key
 sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/debian/$(lsb_release -rs)/amd64/latest/salt-archive-keyring.gpg
 # Create apt sources list file
-echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/debian/$(lsb_release -rs)/amd64/latest $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/salt.list
+sudo echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/debian/$(lsb_release -rs)/amd64/latest $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/salt.list
 
 apt update
 apt install salt-minion -y
